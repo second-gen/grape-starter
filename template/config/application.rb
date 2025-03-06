301c55b17b579e492abe77611f799908c82bb8db
Nathan Wallace <nathan@wallace.fm>
2025-03-06 16:15:22 -0600

    use protocol-agnostic URL for loading OAPI specification

--- template/config/application.rb
+++ template/config/application.rb
@@ -22,7 +22,7 @@ class DocApp
         <style>body {margin: 0;padding: 0;}</style>
       </head>
       <body>
-        <redoc spec-url='http://#{server}:#{port}#{prefix}/#{Api::Base.version}/oapi.json'></redoc>
+        <redoc spec-url='//#{server}:#{port}#{prefix}/#{Api::Base.version}/oapi.json'></redoc>
         <script src='https://rebilly.github.io/ReDoc/releases/latest/redoc.min.js'> </script>
       </body>
     </html>"
